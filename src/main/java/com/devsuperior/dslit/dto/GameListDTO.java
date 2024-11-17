917d37d439129c0f40e07a3b2f490be02b08d2f4
Jeferson Barros <im.jbalves@gmail.com>
2024-11-16 21:35:11 -0300

    get lists

---
+++ src/main/java/com/devsuperior/dslit/dto/GameListDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslit.dto;
+
+import com.devsuperior.dslit.entities.GameList;
+
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+    public GameListDTO() {}
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
